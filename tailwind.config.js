c8ca76be0f403613815b8b9a69171a058af832d1
Mateus Soares <mateus.soares_2013@hotmail.com>
2024-04-01 11:29:36 -0300

    terceiro commit do marcio , tailwind e definiçoes de cores

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    colors: {
+      'orange': '#e28625', 
+      'green': '#886971',
+      'dark-green': '#884148',
+      'offwhite': 'rgba(255, 255, 255, 0.75)',
+      'white': '#fff',
+      'offwhitebg': '#f2f2f2'
+
+
+    }
+  },
+  plugins: [],
+}
+
